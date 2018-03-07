ff793f1cd09859a26c9e4eb5dc0703e52760e6d1
Petar Stanev <petarstanev@outlook.com>
2018-03-07 12:41:15 +0000

    Put working.

--- src/BookReviewBundle/Controller/ApiBookController.php
+++ src/BookReviewBundle/Controller/ApiBookController.php
@@ -63,5 +63,42 @@ class ApiBookController extends FOSRestController
         // along with a 400 status code
             return $this->handleView($form->getErrorsAsString(), 400);
         }
+
+    }
+
+    public function putBookAction($id, Request $request)
+    {
+        // prepare the form and remove the submit button
+        $bookEntry = $this->getDoctrine()->getRepository("BookReviewBundle:Book")->find($id);
+
+        $form = $this->createForm(BookType::class, $bookEntry);
+        // Point 1 of list above
+        if($request->getContentType() != 'json') {
+            return $this->handleView($this->view(null, 400));
+        }
+        // json_decode the request content and pass it to the form
+        $form->submit(json_decode($request->getContent(), true));
+        // Point 2 of list above
+        if($form->isValid()) {
+            // Point 4 of list above
+            $em = $this->getDoctrine()->getManager();
+
+
+            $em->flush();
+            // set status code to 201 and set the Location header
+            // to the URL to retrieve the blog entry - Point 5
+            return $this->handleView($this->view(null, 201)
+                ->setLocation(
+                    $this->generateUrl('book_show',
+                        ['id' => $bookEntry->getId()]
+                    )
+                )
+            );
+        } else {
+            // the form isn't valid so return the form
+            // along with a 400 status code
+            return $this->handleView($form->getErrors(true), 400);
+        }
+
     }
 }
